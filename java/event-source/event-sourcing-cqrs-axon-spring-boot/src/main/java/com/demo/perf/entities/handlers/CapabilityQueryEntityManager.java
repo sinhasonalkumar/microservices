6edf560fe1259701bafb5250dc34a2cdb20660aa
sonal <sinha.sonal.kumar@gmail.com>
2020-04-05 14:42:11 -0700

    for subscribing to query event

--- java/event-source/event-sourcing-cqrs-axon-spring-boot/src/main/java/com/demo/perf/entities/handlers/CapabilityQueryEntityManager.java
+++ java/event-source/event-sourcing-cqrs-axon-spring-boot/src/main/java/com/demo/perf/entities/handlers/CapabilityQueryEntityManager.java
@@ -2,8 +2,7 @@ package com.demo.perf.entities.handlers;
 
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.eventhandling.ResetHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.axonframework.queryhandling.QueryUpdateEmitter;
 import org.springframework.stereotype.Component;
 
 import com.demo.perf.entities.CapabilityQueryEntity;
@@ -18,29 +17,33 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CapabilityQueryEntityManager {
 	
-	//private Logger logger = LoggerFactory.getLogger(CapabilityQueryEntityManager.class);
     
     private CapabilityRepository capabilityRepository;
+    
+    private final QueryUpdateEmitter updateEmitter;
     
     @EventHandler
 	public void createCapability(CapabilityCreatedEvent capabilityCreatedEvent) throws Exception {
     	CapabilityQueryEntity capabilityQueryEntity = new CapabilityQueryEntity(capabilityCreatedEvent.id,capabilityCreatedEvent.name,capabilityCreatedEvent.description,capabilityCreatedEvent.capabilityType);
-    	//logger.info(" CapabilityQueryEntityManager :: CapabilityCreatedEvent :: CapabilityQueryEntity :: " + capabilityQueryEntity);  
+    	  
     	capabilityRepository.save(capabilityQueryEntity);
+    	updateEmitter.emit(m -> "listAllCapability".equals(m.getQueryName()), capabilityQueryEntity);
 	}
     
     @EventHandler
 	public void updateCapability(CapabilityUpdatedEvent capabilityUpdatedEvent) throws Exception {
        	 CapabilityQueryEntity capabilityQueryEntity = new CapabilityQueryEntity(capabilityUpdatedEvent.id,capabilityUpdatedEvent.name,capabilityUpdatedEvent.description, capabilityUpdatedEvent.capabilityType);
-       //	logger.info(" CapabilityQueryEntityManager :: CapabilityUpdatedEvent :: CapabilityQueryEntity :: " + capabilityQueryEntity);
+       
        	 capabilityRepository.save(capabilityQueryEntity);
+       	 updateEmitter.emit(m -> "listAllCapability".equals(m.getQueryName()), capabilityQueryEntity);
    	}
     
     @EventHandler
    	public void deleteCapability(CapabilityDeletedEvent capabilityDeletedEvent) throws Exception {
        	 CapabilityQueryEntity capabilityQueryEntity = new CapabilityQueryEntity(capabilityDeletedEvent.id);
-     	//logger.info(" CapabilityQueryEntityManager :: CapabilityDeletedEvent :: CapabilityQueryEntity :: " + capabilityQueryEntity);
+     	
        	 capabilityRepository.delete(capabilityQueryEntity);
+       	 updateEmitter.emit(m -> "listAllCapability".equals(m.getQueryName()), capabilityQueryEntity);
    	}
     
     @ResetHandler
