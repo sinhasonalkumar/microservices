f968cf8b0979f53511b1e3cdf4e37d30cc955797
sonal <sinha.sonal.kumar@gmail.com>
2020-04-13 14:32:24 -0700

    troubleshooting fixing content type

--- java/istio-demo/capability-service/src/main/java/com/sonal/demo/istio/capabilityservice/externalservices/restclient/DiscoverSkillsServiceClient.java
+++ java/istio-demo/capability-service/src/main/java/com/sonal/demo/istio/capabilityservice/externalservices/restclient/DiscoverSkillsServiceClient.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -19,6 +22,8 @@ import reactor.core.publisher.Mono;
 @Service
 public class DiscoverSkillsServiceClient {
 	
+	private  Logger logger = LoggerFactory.getLogger(DiscoverSkillsServiceClient.class);
+	
 	@Value("${rest.client.baseurl.skillsDiscoveryService}")
 	private String skillsDiscoveryServiceBaseURL ;
 	
@@ -28,12 +33,24 @@ public class DiscoverSkillsServiceClient {
 		return  WebClient.builder()
 				   .baseUrl(skillsDiscoveryServiceBaseURL)
 				   .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				   .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
 				   .build()
 				   .get()
 				   .uri("/discoverSkills/discover")
 				   .accept(MediaType.APPLICATION_JSON)
-				   .exchange()
-				   .flatMap(r -> r.bodyToMono(SkillsDiscoveredDTO.class))
+				   .retrieve()
+				   .onStatus(HttpStatus::is4xxClientError, response -> {
+					   logger.error("4xx eror");
+				        return Mono.error(new RuntimeException("4xx"));
+				      })
+				   .onStatus(HttpStatus::is5xxServerError, response -> {
+					   logger.error("5xx eror");
+					   logger.error("Response Headers :: " + response.headers());
+					   logger.error("Response :: " + response);
+				        return Mono.error(new RuntimeException("5xx"));
+				      })
+				   .bodyToMono(SkillsDiscoveredDTO.class)
+				   .doOnError(e -> logger.error(" ********* Error *******",e))
 				   .map(ds -> ds.getSkillsDiscovered())
 				   .map(s -> mapToCapability(s));
 	}
