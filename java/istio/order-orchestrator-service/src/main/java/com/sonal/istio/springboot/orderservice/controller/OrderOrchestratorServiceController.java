01334f3b55f4325c3eec215686a3719aee95d007
sonal <sinha.sonal.kumar@gmail.com>
2021-03-29 09:45:15 -0600

    for carany deployment test

--- java/istio/order-orchestrator-service/src/main/java/com/sonal/istio/springboot/orderservice/controller/OrderOrchestratorServiceController.java
+++ java/istio/order-orchestrator-service/src/main/java/com/sonal/istio/springboot/orderservice/controller/OrderOrchestratorServiceController.java
@@ -2,6 +2,7 @@ package com.sonal.istio.springboot.orderservice.controller;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,12 @@ public class OrderOrchestratorServiceController {
 		return orderOrchestratorService.placeOrder(orderRequest)
 				                       .map(shippingResponse -> ResponseEntity.ok(shippingResponse));
 		
+	}
+	
+	@GetMapping(value = "/dockerBuildVersion")
+	public Mono<ResponseEntity<String>> dockerBuildVersion(){
+		
+		return Mono.just(ResponseEntity.ok("v2"));
 	}
 	
 }
