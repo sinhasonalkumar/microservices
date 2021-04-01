d670e0bfb0bb27632e1e21c1949500e445b90e35
sonal <sinha.sonal.kumar@gmail.com>
2021-04-01 11:30:27 -0600

    for injecting defect

--- java/istio/order-orchestrator-service/src/main/java/com/sonal/istio/springboot/orderservice/service/FaultyService.java
+++ java/istio/order-orchestrator-service/src/main/java/com/sonal/istio/springboot/orderservice/service/FaultyService.java
@@ -35,7 +35,7 @@ public class FaultyService {
 			
 			log.error(message);
 			
-			return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
                     .body((OrderResponse.builder()
                     .message(message)
                     .build())));
