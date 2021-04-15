bb92c7fa95c1924cb48bb8d932eb747d6cf84f48
sonal <sinha.sonal.kumar@gmail.com>
2021-04-14 18:22:16 -0600

    local and remote token valiation cleanups

--- java/oidc/resource-servers/product-service/src/main/java/com/sonal/oidc/spring/okta/productservice/config/OAuth2ResourceServerSecurityConfiguration.java
+++ java/oidc/resource-servers/product-service/src/main/java/com/sonal/oidc/spring/okta/productservice/config/OAuth2ResourceServerSecurityConfiguration.java
@@ -1,40 +1,26 @@
 package com.sonal.oidc.spring.okta.productservice.config;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2Error;
-import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtIssuerValidator;
-import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.authentication.JwtBearerTokenAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.OpaqueTokenAuthenticationProvider;
-import org.springframework.security.oauth2.server.resource.introspection.NimbusOpaqueTokenIntrospector;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Set;
 
 
 @EnableGlobalMethodSecurity(
@@ -46,15 +32,12 @@ import java.util.Set;
 //Using @EnableResourceServer is deprecated in Spring Boot 2.1. You now configure a resource server using Spring Security’s config
 @Configuration
 public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	
 	@Autowired
-	private OAuth2ClientProperties oAuth2ClientProperties;
+	private JwtDecoder jwtDecoder;
 	
-	//@Autowired
-	//private JwtDecoder jwtDecoder;
-	
-	//@Autowired
-	//private OpaqueTokenIntrospector opaqueTokenIntrospector;
+	@Autowired
+	private OpaqueTokenIntrospector opaqueTokenIntrospector;
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -68,142 +51,48 @@ public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfig
             //.and()
             //.oauth2ResourceServer().jwt(); // replace .jwt() with .opaqueToken() for Opaque Token case
             //.oauth2ResourceServer().opaqueToken();
+        	// These methods are not handy when we have mix of jwt() and opaqueToken() case
        
         // Send a 401 message to the browser (w/o this, you'll see a blank page)
         //Okta.configureResourceServer401ResponseBody(http);
         
-       // configure a custom authentication manager resolver. For dynamically validating JWT token locally or by remote introspection (Opaque Token case) 
+       // configure a custom authentication manager resolver. 
+       //For dynamically validating JWT token locally or by remote introspection (Opaque Token case)
+       // To Get this woring we have to remove okta-spring-boot starter maven dependency 
        http.oauth2ResourceServer().authenticationManagerResolver(customAuthenticationManager());
     }
     
     
-    AuthenticationManagerResolver<HttpServletRequest> customAuthenticationManager() {
+	private AuthenticationManagerResolver<HttpServletRequest> customAuthenticationManager() {
     	
         LinkedHashMap<RequestMatcher, AuthenticationManager> authenticationManagers = new LinkedHashMap<>();
 
         // USE JWT tokens (locally validated) to validate HEAD, GET, and OPTIONS requests
-        List<String> readMethod = Arrays.asList("HEAD", "GET", "OPTIONS");
+        List<String> readMethod = Arrays.asList(HttpMethod.HEAD.name(),
+        										HttpMethod.GET.name(),
+        										HttpMethod.OPTIONS.name());
+        
         RequestMatcher readMethodRequestMatcher = request -> readMethod.contains(request.getMethod());
-        authenticationManagers.put(readMethodRequestMatcher, jwt());
+        authenticationManagers.put(readMethodRequestMatcher, jwtLocalValidation());
 
         // all other requests will use opaque tokens (remotely validated)
         RequestMatchingAuthenticationManagerResolver authenticationManagerResolver = new RequestMatchingAuthenticationManagerResolver(authenticationManagers);
         
-        authenticationManagerResolver.setDefaultAuthenticationManager(opaque());
+        authenticationManagerResolver.setDefaultAuthenticationManager(opaqueTokenValidationByRemoteIntrospection());
         
         return authenticationManagerResolver;
-    }
+    } 
     
-   
-    /*
-    
-    AuthenticationManager jwt() {
+	private AuthenticationManager jwtLocalValidation() {
        
     	JwtAuthenticationProvider authenticationProvider = new JwtAuthenticationProvider(jwtDecoder);
         authenticationProvider.setJwtAuthenticationConverter(new JwtBearerTokenAuthenticationConverter());
         return authenticationProvider::authenticate;
     }
 
-    AuthenticationManager opaque() {
+	private AuthenticationManager opaqueTokenValidationByRemoteIntrospection() {
         
         return new OpaqueTokenAuthenticationProvider(opaqueTokenIntrospector)::authenticate;
-    }
-    
-    
-    
-    @Bean
-    JwtDecoder jwtDecoder() {
-    	// this is the keys endpoint for okta
-        String issuer = oAuth2ClientProperties.getProvider().get("okta").getIssuerUri();
-        String jwkSetUri = issuer + "/v1/keys";
-
-        NimbusJwtDecoder nimbusJwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
-
-        // okta recommends validating the `iss` and `aud` claims
-        // see: https://developer.okta.com/docs/guides/validate-access-tokens/java/overview/
-        List<OAuth2TokenValidator<Jwt>> validators = new ArrayList<>();
-        validators.add(new JwtTimestampValidator());
-        validators.add(new JwtIssuerValidator(issuer));
-        validators.add(token -> {
-            Set<String> expectedAudience = new HashSet<>();
-            expectedAudience.add("api://default"); // this is the default value, update this accordingly
-            return !Collections.disjoint(token.getAudience(), expectedAudience)
-                    ? OAuth2TokenValidatorResult.success()
-                    : OAuth2TokenValidatorResult.failure(new OAuth2Error(OAuth2ErrorCodes.INVALID_REQUEST,
-                        "This aud claim is not equal to the configured audience",
-                        "https://tools.ietf.org/html/rfc6750#section-3.1"));
-        });
-        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(validators);
-        nimbusJwtDecoder.setJwtValidator(validator);
-        
-        return nimbusJwtDecoder;
-    }
-    
-    @Bean
-    OpaqueTokenIntrospector opaqueTokenIntrospector() {
-    	
-    	 String issuer = oAuth2ClientProperties.getProvider().get("okta").getIssuerUri();
-         String introspectionUri = issuer + "/v1/introspect";
-
-         // The default opaque token logic
-         OAuth2ClientProperties.Registration oktaRegistration = oAuth2ClientProperties.getRegistration().get("okta");
-         OpaqueTokenIntrospector introspectionClient = new NimbusOpaqueTokenIntrospector(
-                 introspectionUri,
-                 oktaRegistration.getClientId(),
-                 oktaRegistration.getClientSecret());
-         
-         return introspectionClient;
-    }
-    */
-    
-    
-    AuthenticationManager jwt() {
-        // this is the keys endpoint for okta
-        String issuer = oAuth2ClientProperties.getProvider().get("okta").getIssuerUri();
-        String jwkSetUri = issuer + "/v1/keys";
-
-        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
-
-        // okta recommends validating the `iss` and `aud` claims
-        // see: https://developer.okta.com/docs/guides/validate-access-tokens/java/overview/
-        List<OAuth2TokenValidator<Jwt>> validators = new ArrayList<>();
-        validators.add(new JwtTimestampValidator());
-        validators.add(new JwtIssuerValidator(issuer));
-        validators.add(token -> {
-            Set<String> expectedAudience = new HashSet<>();
-            expectedAudience.add("com.okta.oidc.svc.product"); // this is the default value, update this accordingly
-            return !Collections.disjoint(token.getAudience(), expectedAudience)
-                    ? OAuth2TokenValidatorResult.success()
-                    : OAuth2TokenValidatorResult.failure(new OAuth2Error(OAuth2ErrorCodes.INVALID_REQUEST,
-                        "This aud claim is not equal to the configured audience",
-                        "https://tools.ietf.org/html/rfc6750#section-3.1"));
-        });
-        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(validators);
-        jwtDecoder.setJwtValidator(validator);
-
-        JwtAuthenticationProvider authenticationProvider = new JwtAuthenticationProvider(jwtDecoder);
-        authenticationProvider.setJwtAuthenticationConverter(new JwtBearerTokenAuthenticationConverter());
-        return authenticationProvider::authenticate;
-    }
-
-    AuthenticationManager opaque() {
-        String issuer = oAuth2ClientProperties.getProvider().get("okta").getIssuerUri();
-        String introspectionUri = issuer + "/v1/introspect";
-
-        // The default opaque token logic
-        OAuth2ClientProperties.Registration oktaRegistration = oAuth2ClientProperties.getRegistration().get("okta");
-        OpaqueTokenIntrospector introspectionClient = new NimbusOpaqueTokenIntrospector(
-                introspectionUri,
-                oktaRegistration.getClientId(),
-                oktaRegistration.getClientSecret());
-        return new OpaqueTokenAuthenticationProvider(introspectionClient)::authenticate;
-    }
-    
-    
-    @Bean
-    OAuth2ClientProperties oAuth2ClientProperties() {
-        return new OAuth2ClientProperties();
-    }
-    
+    } 
 
 }
