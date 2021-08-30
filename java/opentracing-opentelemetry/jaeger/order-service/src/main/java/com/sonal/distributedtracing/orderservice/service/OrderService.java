2db5313aa1fbcf493f388555b2147d3c0fee43f0
sonal <sinha.sonal.kumar@gmail.com>
2021-08-30 00:39:09 -0600

    trying to get stackTrace parsed by fluent bit in log_processed

--- java/opentracing-opentelemetry/jaeger/order-service/src/main/java/com/sonal/distributedtracing/orderservice/service/OrderService.java
+++ java/opentracing-opentelemetry/jaeger/order-service/src/main/java/com/sonal/distributedtracing/orderservice/service/OrderService.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 import com.sonal.distributedtracing.orderservice.annotation.LogTimeElapsed;
 import com.sonal.distributedtracing.orderservice.client.ShippingServiceClient;
 import com.sonal.distributedtracing.orderservice.client.vo.ShippingResponseVO;
@@ -13,6 +15,7 @@ import com.sonal.distributedtracing.orderservice.vo.OrderResponseVO;
 
 import brave.Tracer;
 import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.encoder.org.apache.commons.lang3.exception.ExceptionUtils;
 
 @Slf4j
 @Service
@@ -33,12 +36,34 @@ public class OrderService {
 		
 		tracer.currentSpan().tag("orderId", orderId);
 		
-		OrderResponseVO build = OrderResponseVO.builder()
-				  .orderId(orderId)
-	              .message("Order Successfully Placed")
-	              .build();
+		OrderResponseVO build = null;
 		
-		ShippingResponseVO shipment = shippingServiceClient.ship(orderRequestVO);
+		try {
+			ShippingResponseVO shipment = shippingServiceClient.ship(orderRequestVO);
+			build = OrderResponseVO.builder()
+					  .orderId(orderId)
+		              .message("Order Successfully Placed")
+		              .build();
+		} catch (Exception e) {
+			
+			log.error("{\"error\" : \"Error While Calling Shipping-Service \"}");
+			
+			String stacktrace = ExceptionUtils.getStackTrace(e);
+			
+			log.error("Error While Calling Shipping-Service - stackTrace = " + stacktrace);
+			
+			log.error("Error While Calling Shipping-Service KV ", kv("stackTrace", stacktrace));
+			
+			log.error("Error While Calling Shipping-Service ");
+			
+			log.error("Error While Calling Shipping-Service Regular",e);
+			
+			build = OrderResponseVO.builder()
+					  .orderId(orderId)
+		              .message("Order Failed To Place")
+		              .build();
+		}
+		
 		
 		return build;
 	}
